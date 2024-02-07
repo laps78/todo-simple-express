e8ee9836be37bbfe88735520826117cea1d0e3ef
laps78 <sovtem@gmail.com>
2024-02-07 16:06:50 +0300

    починил отправку данных с помощью формы

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const errorMW = require('./middleware/error')
-const dotenv = require('dotenv')
+const express = require("express");
+const errorMW = require("./middleware/error");
+const dotenv = require("dotenv");
 
-const indexRouter = require('./routes/index.route')
-const todoRouter = require('./routes/todo.route')
+const indexRouter = require("./routes/index.route");
+const todoRouter = require("./routes/todo.route");
 
-dotenv.config()
-const app = express()
-//app.use(express.urlencoded())
-app.set("view engine", "ejs")
+dotenv.config();
+const app = express();
+app.use(express.urlencoded());
+app.set("view engine", "ejs");
 
-app.use('/', indexRouter)
-app.use('/todo', todoRouter)
+app.use("/", indexRouter);
+app.use("/todo", todoRouter);
 
-app.use(errorMW)
+app.use(errorMW);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT) && console.log(`app sucsessfully started at localhost:${PORT}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT) &&
+  console.log(`app sucsessfully started at localhost:${PORT}`);
