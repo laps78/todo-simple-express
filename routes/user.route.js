d1eaa55e2b04d51b0eb9da4167cff3c97e406e05
laps78 <sovtem@gmail.com>
2024-02-16 13:43:33 +0300

    развернута структура роута api/user

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.use((req, res) => {
+router.use("/test", (req, res) => {
   res.json("it works!");
 });
+
+router.get("/login", (req, res) => {});
+
+router.get("/me", (req, res) => {});
+
+router.post("/login", (req, res) => {});
+
+router.post("/signup", (req, res) => {});
 
 module.exports = router;
